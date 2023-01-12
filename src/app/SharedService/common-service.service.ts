c431081c6da206e81e101904dd407d82e8a88388
harsha-kaithal <harshakaithal@gmail.com>
2023-01-12 21:55:51 +0530

    push

--- src/app/SharedService/common-service.service.ts
+++ src/app/SharedService/common-service.service.ts
@@ -4,18 +4,33 @@ import { BehaviorSubject } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
+
 export class CommonServiceService {
+
   sourceValue = new BehaviorSubject("");
+
   currentValue=this.sourceValue.asObservable();
+
   userData:any;
+
   constructor() { }
 
+
+
   public changeValue(value:string){
+
     this.sourceValue.next(value);
+
   }
 
+
+
   getUserData(user:any)
+
   {
+
     this.userData=user;
+
   }
+
 }
